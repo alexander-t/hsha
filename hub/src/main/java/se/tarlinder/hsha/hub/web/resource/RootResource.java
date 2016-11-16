75f1e091e820cc269afaeae70cf1458923b39879
alexander <alexander.tarlinder@crisp.se>
2016-11-16 15:39:04 +0100

    Read build time from buildInfo.properties.

--- hub/src/main/java/se/tarlinder/hsha/hub/web/resource/RootResource.java
+++ hub/src/main/java/se/tarlinder/hsha/hub/web/resource/RootResource.java
@@ -1,20 +1,35 @@
 package se.tarlinder.hsha.hub.web.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.Properties;
 
 @Component
 @Path("/")
 public class RootResource {
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @GET
     @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
     public String index() {
-        return "HSHA Hub up and running";
+        Resource resource = resourceLoader.getResource("classpath:buildInfo.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(resource.getInputStream());
+            return String.format("HSHA Hub built %s", (String) properties.get("time"));
+        } catch (IOException e) {
+            return "HSHA Hub (build info missing!)";
+        }
     }
 }
