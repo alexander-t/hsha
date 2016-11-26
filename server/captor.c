79a57d6d8faa3d5d5354800374b3958433b582f9
Alexander Tarnowski <alexander.tarnowski@crisp.se>
2016-11-26 22:04:32 +0100

    Added a mutex around the printing to stdout for good measure.

--- server/captor.c
+++ server/captor.c
@@ -5,27 +5,38 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <pthread.h> 
 #include <telldus-core.h>
 
 GHashTable *data_type2unit;
 GHashTable *device_id2name;
 
+/* Defensive: it's not obvious whether the device and sensor callbacks are handled
+   by one or two threads. Adding a mutex as a precaution should prevent garbled
+   output.
+*/
+pthread_mutex_t stdout_mutex;
+
 void sensor_callback(const char *protocol, const char *model,
 		     int sensor_id, int data_type, const char *value,
 		     int timestamp, int callback_id, void *context)
 {
+   pthread_mutex_lock(&stdout_mutex);
    printf("S%d,%s,%s\n", sensor_id,
 	  (char *) g_hash_table_lookup(data_type2unit, GINT_TO_POINTER(data_type)),
 	  value);
+   pthread_mutex_unlock(&stdout_mutex);
 }
 
 
 void device_callback(int device_id, int method, const char *data,
 		     int callback_id, void *context)
 {
+   pthread_mutex_lock(&stdout_mutex);
    printf("D%d,%s,%s\n", device_id,
 	  method == TELLSTICK_TURNON ? "on" : "off",
 	  (char *) g_hash_table_lookup(device_id2name, GINT_TO_POINTER(device_id)));
+   pthread_mutex_unlock(&stdout_mutex);
 }
 
 
@@ -66,6 +77,12 @@ int main(int argc, char* argv[])
    setbuf(stdout, NULL);
          
    tdInit();
+
+   if (pthread_mutex_init(&stdout_mutex, NULL) != 0)
+   {
+      fprintf(stderr, "Failed to initialize stdout mutex!\n");
+      return 1;
+   }
    
    data_type2unit = create_unit_table();
    device_id2name = create_name_table();
@@ -73,7 +90,7 @@ int main(int argc, char* argv[])
    tdRegisterSensorEvent(&sensor_callback, NULL);
    tdRegisterDeviceEvent(&device_callback, NULL);
       
-   while(1)
+   while (1)
    {
       sleep(10);
    }  
